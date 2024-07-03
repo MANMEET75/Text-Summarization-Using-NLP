9a87d3c8ac0f68b5662d431b8433ae242b9817dd
MANMEET SINGH (AWM) <97391884+MANMEET75@users.noreply.github.com>
2024-07-03 22:42:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "MANMEET75"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "entbappy73@gmail.com"
 
